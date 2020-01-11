21a0027e4465416dd0d8ec74c2843f68b5811314
Larsen Morehouse <larsenmae@gmail.com>
2020-01-11 15:33:21 -0500

    Initial Subsystem Development

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.Chassis;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.ExampleCommand;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Chassis chassis = new Chassis();
+
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  // The driver's controller
+  XboxController m_controller = new XboxController(OIConstants.kDriverControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    // Configure default commands
+    // Set the default drive command to split-stick arcade drive
+    // A split-stick arcade command, with forward/backward controlled by the left
+    // hand, and turning controlled by the right.
+    chassis.setDefaultCommand(new RunCommand(
+        () -> chassis.drive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getX(GenericHID.Hand.kRight)),
+        chassis));
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SpeedController;
+import frc.robot.Constants.ShooterConstants;
+import edu.wpi.first.wpilibj.SpeedController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Add your docs here.
+ */
+public class Shooter extends SubsystemBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  // @Override
+  // public void initDefaultCommand() {
+  // // Set the default command for a subsystem here.
+  // // setDefaultCommand(new MySpecialCommand());
+  // }
+  // private final SpeedController m_leftMaster = leftMaster;
+  private final CANSparkMax shooter = new CANSparkMax(ShooterConstants.kShooterMotor, MotorType.kBrushless);
+  private final SpeedController shootMotor = shooter;
+  private final CANSparkMax angler = new CANSparkMax(ShooterConstants.kShooterMotor, MotorType.kBrushless);
+  private final SpeedController angleMotor = angler;
+  private final CANSparkMax tilter = new CANSparkMax(ShooterConstants.kShooterMotor, MotorType.kBrushless);
+  private final SpeedController tiltMotor = tilter;
+
+  private final PIDController PIDController = new PIDController(ShooterConstants.kP, ShooterConstants.kI,
+      ShooterConstants.kD);
+
+  public Shooter() {
+
+  }
+
+  public void shooterShoot() {
+
+  }
+
+  public void moveToAngle() {
+
+  }
+
+  public void setAngle() {
+
+  }
+
+  public void setTilt() {
+
+  }
+
+  public void moveToTilt() {
+
+  }
+
+}
