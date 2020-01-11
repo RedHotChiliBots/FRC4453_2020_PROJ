e7111ec04262f5a9eeddf0faf32460921ebdf76e
Larsen Morehouse <larsenmae@gmail.com>
2020-01-11 15:33:21 -0500

    Initial Subsystem Development

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+	public static final class UnitsConstants {
+		public static final double kF2M = 0.3048;
+	}
+
+	public static final class OIConstants {
+		public static final int kDriverControllerPort = 0;
+		public static final int kOperatorControllerPort = 1;
+	}
+
+	public static final class ChassisConstants {
+		public static final int kLeftMasterMotor = 0;
+		public static final int kLeftFollowerMotor = 1;
+		public static final int kRightMasterMotor = 2;
+		public static final int kRightFollowerMotor = 3;
+
+		public static final double kMaxSpeedFPS = 10.0; // feet per second
+		public static final double kMaxSpeedMPS = kMaxSpeedFPS * UnitsConstants.kF2M // meters per second
+		public static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation per second
+
+		public static final double kTrackWidth = 1.5 * UnitsConstants.kF2M * 2; // meters
+		public static final double kWheelDiameter = 6.0 * UnitsConstants.kF2M;
+		public static final double kWheelRadius = kWheelDia / 2.0;
+		public static final int kEncoderResolution = 4096;
+
+		public static final double kP = 1;
+		public static final double kI = 0;
+		public static final double kD = 0;
+	}
+
+	public final static class ShooterConstants {
+		public static final int kShooterMotor = 10;
+		public static final int kAngleMotor = 11;
+		public static final int kTiltMotor = 12;
+
+		public static final double kP = 1;
+		public static final double kI = 0;
+		public static final double kD = 0;
+	}
+
+	public final static class SpinnerConstants {
+		public static final int kSpinnerMotor = 20;
+
+		public static final double kP = 1;
+		public static final double kI = 0;
+		public static final double kD = 0;
+	}
+}
---
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -0,0 +1,134 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj.AnalogGyro;
+// import edu.wpi.first.wpilibj.Encoder;
+// import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Constants.ChassisConstants;
+
+/**
+ * Represents a differential drive style drivetrain.
+ */
+public class Chassis extends SubsystemBase {
+
+	private final CANSparkMax leftMaster = new CANSparkMax(ChassisConstants.kLeftMasterMotor, MotorType.kBrushless);
+	private final SpeedController m_leftMaster = leftMaster;
+	private final SpeedController m_leftFollower = new CANSparkMax(ChassisConstants.kLeftFollowerMotor,
+			MotorType.kBrushless);
+	private final CANSparkMax rightMaster = new CANSparkMax(ChassisConstants.kRightMasterMotor, MotorType.kBrushless);
+	private final SpeedController m_rightMaster = rightMaster;
+	private final SpeedController m_rightFollower = new CANSparkMax(ChassisConstants.kRightFollowerMotor,
+			MotorType.kBrushless);
+
+	// private final Encoder m_leftEncoder = new Encoder(0, 1);
+	private final CANEncoder m_leftEncoder = new CANEncoder(leftMaster);
+	private final CANEncoder m_rightEncoder = new CANEncoder(rightMaster);
+	// private final Encoder m_rightEncoder = new Encoder(2, 3);
+
+	private final SpeedControllerGroup m_leftGroup = new SpeedControllerGroup(m_leftMaster, m_leftFollower);
+	private final SpeedControllerGroup m_rightGroup = new SpeedControllerGroup(m_rightMaster, m_rightFollower);
+
+	/* Alternatively: I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB */
+	private final AHRS ahrs = new AHRS(SPI.Port.kMXP);
+	// private final AnalogGyro m_gyro = new AnalogGyro(0);
+
+	private final PIDController m_rightPIDController = new PIDController(ChassisConstants.kP, ChassisConstants.kI,
+			ChassisConstants.kD);
+	private final PIDController m_leftPIDController = new PIDController(ChassisConstants.kP, ChassisConstants.kI,
+			ChassisConstants.kD);
+	// private final PIDController m_rightPIDController = new PIDController(1, 0,
+	// 0);
+
+	private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(
+			ChassisConstants.kTrackWidth);
+
+	private final DifferentialDriveOdometry m_odometry;
+
+	/**
+	 * Constructs a differential drive object. Sets the encoder distance per pulse
+	 * and resets the gyro.
+	 */
+	public Chassis() {
+		ahrs.reset();
+
+		// Set the distance per pulse for the drive encoders. We can simply use the
+		// distance traveled for one rotation of the wheel divided by the encoder
+		// resolution.
+		m_leftEncoder.setPositionConversionFactor(2 * Math.PI * ChassisConstants.kWheelRadius);
+
+		// .setDistancePerPulse(2 * Math.PI * ChassisConstants.kWheelRadius /
+		// ChassisConstants.kEncoderResolution);
+		m_rightEncoder.setPositionConversionFactor(2 * Math.PI * ChassisConstants.kWheelRadius);
+		// .setDistancePerPulse(2 * Math.PI * ChassisConstants.kWheelRadius /
+		// ChassisConstants.kEncoderResolution);
+
+		// m_leftEncoder.reset();
+		// m_rightEncoder.reset();
+
+		m_odometry = new DifferentialDriveOdometry(getAngle());
+	}
+
+	/**
+	 * Returns the angle of the robot as a Rotation2d.
+	 *
+	 * @return The angle of the robot.
+	 */
+	public Rotation2d getAngle() {
+		// Negating the angle because WPILib gyros are CW positive.
+		return Rotation2d.fromDegrees(-ahrs.getAngle());
+	}
+
+	/**
+	 * Sets the desired wheel speeds.
+	 *
+	 * @param speeds The desired wheel speeds.
+	 */
+	public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+		double leftOutput = m_leftPIDController.calculate(m_leftEncoder.getVelocity(), speeds.leftMetersPerSecond);
+		double rightOutput = m_rightPIDController.calculate(m_rightEncoder.getVelocity(), speeds.rightMetersPerSecond);
+		m_leftGroup.set(leftOutput);
+		m_rightGroup.set(rightOutput);
+	}
+
+	/**
+	 * Drives the robot with the given linear velocity and angular velocity.
+	 *
+	 * @param xSpeed Linear velocity in m/s.
+	 * @param rot    Angular velocity in rad/s.
+	 */
+	// @SuppressWarnings("ParameterName")
+	public void drive(double xSpeed, double rot) {
+		var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+		setSpeeds(wheelSpeeds);
+	}
+
+	/**
+	 * Updates the field-relative position.
+	 */
+	public void updateOdometry() {
+		m_odometry.update(getAngle(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+	}
+
+}
