f49d12fa47ca6dd7dfba1460e70ef86377a75de2
sdseifert <sseifert@redhotchilibots.com>
2020-01-23 23:17:53 -0500

    Reset sticky faults on PDP during startup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,6 +28,9 @@ public class Robot extends TimedRobot {
 		// Instantiate our RobotContainer. This will perform all our button bindings,
 		// and put our
 		// autonomous chooser on the dashboard.
+
+		PowerDistributionPanel PDP = new PowerDistributionPanel(0);
+		PDP.clearStickyFaults();
 
 		m_robotContainer = new RobotContainer();
 	}
