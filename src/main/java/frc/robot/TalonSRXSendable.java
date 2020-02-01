68a9fe32af0203fa5a502fd524c63fb77f72c1a4
Conner Ebbinghaus <connerebbinghaus@gmail.com>
2020-01-31 19:52:26 -0500

    Hopefully fix it for real.

--- src/main/java/frc/robot/TalonSRXSendable.java
+++ src/main/java/frc/robot/TalonSRXSendable.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
@@ -13,14 +12,14 @@ public class TalonSRXSendable implements Sendable {
     public TalonSRXSendable(TalonSRX t) {
         talon = t;
     }
-
     @Override
     public void initSendable(SendableBuilder builder) {
-        builder.setSmartDashboardType("Speed Controller");
+        builder.setSmartDashboardType("PIDController");
+
         builder.setActuator(true);
         builder.setSafeState(() -> talon.set(ControlMode.Disabled, 0));
 
-        builder.addDoubleProperty("P", () -> {
+        builder.addDoubleProperty("p", () -> {
             SlotConfiguration c = new SlotConfiguration();
             talon.getSlotConfigs(c);
             return c.kP;
@@ -28,7 +27,7 @@ public class TalonSRXSendable implements Sendable {
             talon.config_kP(0, p);
         });
 
-        builder.addDoubleProperty("I", () -> {
+        builder.addDoubleProperty("i", () -> {
             SlotConfiguration c = new SlotConfiguration();
             talon.getSlotConfigs(c);
             return c.kI;
@@ -36,23 +35,13 @@ public class TalonSRXSendable implements Sendable {
             talon.config_kI(0, p);
         });
 
-        builder.addDoubleProperty("D", () -> {
+        builder.addDoubleProperty("d", () -> {
             SlotConfiguration c = new SlotConfiguration();
             talon.getSlotConfigs(c);
             return c.kD;
         }, (double p) -> {
             talon.config_kD(0, p);
         });
-
-        builder.addDoubleProperty("F", () -> {
-            SlotConfiguration c = new SlotConfiguration();
-            talon.getSlotConfigs(c);
-            return c.kF;
-        }, (double p) -> {
-            talon.config_kF(0, p);
-        });
-
-        builder.addDoubleProperty("Position", () -> talon.getSelectedSensorPosition(), (double p) -> {});
     }
 
 	
